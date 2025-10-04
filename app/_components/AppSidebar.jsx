3f752ddd76bd2c414598c8ac18bd8cd989ea0a03
anurag jaiswal <anuraggjaiswal@gmail.com>
2025-10-04 10:01:02 +0530

    Authentication

--- app/_components/AppSidebar.jsx
+++ app/_components/AppSidebar.jsx
@@ -7,12 +7,15 @@ import {
     SidebarGroup,
     SidebarHeader,
 } from "@/components/ui/sidebar"
-import { Moon, Sun } from "lucide-react"
+import { SignIn, SignInButton, useUser } from "@clerk/nextjs"
+import { Bolt, Moon, Sun, User2, Zap } from "lucide-react"
 import { useTheme } from "next-themes"
 import Image from "next/image"
+import UsageCreditProgress from "./UsageCreditProgress"
 
 export function AppSidebar() {
     const { theme, setTheme } = useTheme();
+    const { user } = useUser();
     return (
         <Sidebar>
             <SidebarHeader>
@@ -28,21 +31,36 @@ export function AppSidebar() {
                                 <Button variant={'ghost'} onClick={() => setTheme('light')}><Moon /></Button>}
                         </div>
                     </div>
-                    <Button className="mt-7 w-full " size="lg" >+ New chat</Button>
+                    {user?
+                    <Button className="mt-7 w-full " size="lg" >+ New chat</Button>:
+                    <SignInButton mode="modal" className="mt-7 w-full ">
+                        <Button className={'w-full'} size={'lg'} >Sign In/Sign Up</Button>
+                    </SignInButton>}
                 </div>
             </SidebarHeader>
             <SidebarContent>
                 <SidebarGroup>
                     <div className="p-3">
                         <h2 className="font-bold text-lg " > Chats</h2>
-                        <p className="text-sm text-gray-400">Sign in to chat with multiple ai models.</p>
+                       {!user&&<p className="text-sm text-gray-400">Sign in to chat with multiple ai models.</p>}
                     </div>
                 </SidebarGroup>
 
             </SidebarContent>
             <SidebarFooter>
                 <div className="p-3 mb-10">
-                    <Button className={'w-full'} size={'lg'} >Sign In/Sign Up</Button>
+                    {!user ? <SignInButton mode="modal">
+                        <Button className={'w-full'} size={'lg'} >Sign In/Sign Up</Button>
+                    </SignInButton>
+                        :
+                        <div>
+                            <UsageCreditProgress/>
+                            <Button className={'mb-3 w-full '}><Zap /> Upgrade Plan </Button>
+                            <Button className="flex " variant={'ghost'}>
+                                <User2 /><h1>Settings</h1>
+                            </Button>
+                        </div>
+                    }
                 </div>
             </SidebarFooter>
         </Sidebar>
--- app/provider.jsx
+++ app/provider.jsx
@@ -1,13 +1,51 @@
-import React from 'react'
+'use client'
+import React, { useEffect } from 'react'
 import { ThemeProvider as NextThemeProvider } from 'next-themes'
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 import { AppSidebar } from './_components/AppSidebar'
 import AppHeader from './_components/AppHeader'
+import { useUser } from '@clerk/nextjs'
+import { getDoc, setDoc, doc } from 'firebase/firestore'
+import { db } from '@/config/FirebaseConfig'
 
 function Provider({
     children,
     ...props
 }) {
+    const { user } = useUser();
+
+    useEffect(() => {
+        if (user) {
+            CreateNewUser();
+        }
+    }, [user])
+
+
+    const CreateNewUser = async () => {
+        //if user is exist
+        const userRef = doc(db, "users", user?.primaryEmailAddress?.emailAddress);
+        const docSnap = await getDoc(userRef);
+
+        if (docSnap.exists()) {
+            console.log("User already exists");
+            return;
+        } else {
+            const userData = {
+                name: user?.fullName,
+                email: user?.primaryEmailAddress?.emailAddress,
+                createdAt: new Date(),
+                remainMsg: 5, //free tier
+                plan: 'free',
+                credits: 1000  //paid tier
+            }
+            await setDoc(userRef, userData);
+            console.log("New user created");
+        }
+
+
+        //if not 
+
+    }
     return (
         <NextThemeProvider
             attribute="class"
